1ea47233f507d6b0e959b50d2095c0616b8b2db0
BuildTools <unconfigured@null.spigotmc.org>
2022-04-15 17:58:20 +0400

    Ajout des fichiers du serveur

---
+++ Login/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+		<title>Masssiwa</title>
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+		<link rel="icon" type="image/ico" href="../favicon.ico">
+	</head>
+	
+	<body>
+		<form>
+			<h1>Se Connecter</h1>
+			<input type="text" name="login" placeholder="entrer votre identifiant">
+			<input type="password" name="mot_de_passe" placeholder="entrer votre mot de passe">
+			<input type="submit" name="valide" value="valider">
+			<input type="reset" name="login" value="annuler" onclick=window.location.href="../index.html"></br>
+			<input type="reset" name="sign up" value="s'inscrire" onclick=window.location.href="../Inscription/index.html">
+		</form>
+	</body>
+</html>
